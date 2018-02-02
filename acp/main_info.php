7fbc176c0f2949edcdd8c1b9b001b84667bf3123
Daniel Mota <leinad4mind@gmail.com>
2018-02-02 08:53:06 +0000

    Update main_info.php

--- acp/main_info.php
+++ acp/main_info.php
@@ -9,15 +9,14 @@
 namespace bruninoit\usersetting\acp;
 class main_info
 {
-function module()
-{
-return array(
-'filename'	=> '\bruninoit\usersetting\acp\main_module',
-'title'	=> 'ACP_US_TITLE',
-'version'	=> '0.1.0-a1',
-'modes'	=> array(
-'settings'	=> array('title' => 'ACP_US_TITLE', 'auth' => 'ext_bruninoit/usersetting && acl_a_board', 'cat' => array('ACP_US_TITLE')),
-),
-);
-}
+  function module() {
+    return array(
+      'filename'	=> '\bruninoit\usersetting\acp\main_module',
+      'title'	=> 'ACP_US_TITLE',
+      'version'	=> '0.1.0-a1',
+      'modes'	=> array(
+      'settings'	=> array('title' => 'ACP_US_TITLE', 'auth' => 'ext_bruninoit/usersetting && acl_a_board', 'cat' => array('ACP_US_TITLE')),
+    ),
+  );
+  }
 }
